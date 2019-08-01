6b3d1893b3e1c4c6d3b8d0eb3564c67a605db72c
shiyueyoulan <1141454529@qq.com>
2019-08-01 23:02:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,34 +4,21 @@ import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import  thunk  from 'redux-thunk'
-import Auth from './Auth'
-import Dashboard from './Dashboard'
 import reducers from './Reducer'
 import './config'
-import 'antd-mobile/dist/antd-mobile.css'
+import Register from './container/regist/register'
+import Login from './container/login/login'
 const store = createStore(
   reducers, 
   compose(applyMiddleware(thunk), window.devToolsExtension?window.devToolsExtension():f=>f)
 )
 
-
-function Test() {
-  return(
-    <div></div>
-  )
-}
 ReactDom.render(
   <Provider store={store}>
-    {/* <App /> */}
     <BrowserRouter>
-      <Switch>
-        <Route path='/login' component={Auth}></Route>
-        <Route path='/dashboard' component={Dashboard}></Route>
-        <Redirect to='/dashboard'></Redirect>
-      </Switch>
       <div>
-      
-      
+        <Route path='/login' component={Login}></Route>
+        <Route path='/register' component={Register}></Route>
       </div>
     </BrowserRouter>
   </Provider>,
