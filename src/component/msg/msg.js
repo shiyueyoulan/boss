f158dc91d6e4ca6dbba2ac27517a68b7ed397023
shiyueyoulan <1141454529@qq.com>
2019-08-07 09:52:11 +0800

    未读消息

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { List } from 'antd-mobile'
+import { List, Badge } from 'antd-mobile'
 
 @connect(
   state=>state
@@ -19,18 +19,31 @@ class Msg extends Component {
       msgGroup[v.chatid] = msgGroup[v.chatid] || []
       msgGroup[v.chatid].push(v)
     })
-    const chatList = Object.values(msgGroup)
+    const chatList = Object.values(msgGroup).sort((a,b)=>{
+      const a_last = this.getLast(a).create_time
+      const b_last = this.getLast(b).create_time
+      return b_last - a_last
+    })
     return (
       <div>
         <List>
           {chatList.map(v=>{
             const lastItem = this.getLast(v)
             const targetId = v[0].from == userid?v[0].to:v[0].from
+            const unreadNum = v.filter(v=>!v.read && v.to==userid).length
             const name = this.props.chat.users[targetId]?this.props.chat.users[targetId].name:null
             const avatar = this.props.chat.users[targetId]?this.props.chat.users[targetId].avatar:null
+            if(!this.props.chat.users[targetId]){
+              return null
+            }
             return (
               <Item key = {lastItem._id}
-                  // thumb = {require(`../images/${avatar}.png`)}
+                extra={<Badge text={unreadNum}></Badge>}
+                thumb = {require(`../images/${avatar}.png`)}
+                arrow="horizontal"
+                onClick={()=>{
+                  this.props.history.push(`/chat/${targetId}`)
+                }}
               >
                 {lastItem.content}
                 <Brirf>{name}</Brirf>
